f65b609c5ae04e9893f5a8cf9caa12d6149a2972
Alexey Solonenko <ot2@bk.ru>
2017-06-04 21:20:49 +0200

    refactor: Make first 2 lessons

---
+++ app.js
@@ -0,0 +1,24 @@
+/*
+
+For this quiz, can you use this script, which is linked in the <head> of index.html,
+to change the boring placeholder image to a picture of a cute animal?
+
+Remember, you'll need to pass a function into the jQuery object to run
+when the document is ready.
+
+Unfortunately, placepuppy is no longer available. Here's a link to a random
+animal image on lorempixel.com:
+http://lorempixel.com/350/150/animals/
+
+Good luck!
+
+*/
+function pictureChanger(url){
+  console.log( "ready here!" );
+  var imgToChange = $('.articles').find('img').first();
+  console.log($('.articles').find('img').html());
+  console.log(imgToChange.text());
+  console.log(imgToChange);
+  imgToChange.attr('src',url);
+  
+};
